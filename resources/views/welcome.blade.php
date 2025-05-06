98f1d9e5e9d1f76392b7ec45b041a20dd665b1a7
ka1danbekov <ka1danbekov_@MacBook-Pro-Bakdaulet.local>
2025-05-06 05:04:19 +0500

    Your commit message describing the changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartKasip</title>
     <link rel="icon" href="../images/logotip.jpeg" type="image/jpeg" class="logotip">
-    <link rel="stylesheet" href="{{ asset('css/style_main.css') }}">
+    <link rel="stylesheet" href="../css/style_main.css">
 </head>
 
 <body>
